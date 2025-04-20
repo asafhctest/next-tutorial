960acab6395f53bcbbe2ff3bae1feefde866068d
Asaf Cohen <asafc@me.com>
2025-04-20 20:33:14 +0300

    tutorial

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <>
+    {/*<div className="flex flex-col p-6">*/}
+        <p>Dashboard Page</p>
+        {/* <Link href="/dashboard/invoices">Invoices</Link>
+        <Link href="/dashboard/customers">Customers</Link> */}
+    {/*</div>*/}
+    </>
+  );
+}
